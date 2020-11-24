2941e19cb4a1cda7ba2cb9e6a908d458d58c7705
Hman-dev <hermannflori@gmail.com>
2020-11-24 23:00:15 +0100

    4-4-3

--- application/views/blog/index.php
+++ application/views/blog/index.php
@@ -6,6 +6,16 @@
     </div>
   </div>
   <div class="row">
+    <div class="col-md-10">
+      <ul class="nav nav-pills nav-justified">
+        <?php if ($this->auth_user->is_connected) : ?>
+          <li role="presentation">
+            <?= anchor('blog/nouvel_article', "Nouvel article"); ?>
+          </li>
+        <?php endif; ?>
+      </ul>
+    </div>
+  <div class="row">
     <div class="col-md-12">
       <p class="text-right">Nombre d'articles : <?= $this->articles->num_items; ?></p>
     </div>
